2831785608002b39c61baaf9832abcf27607eb00
santosharge <santosharge1990@gmail.com>
2025-03-13 11:22:21 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,11 +1,12 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
+      version = "5.90.1"
     }
   }
 }
 
 provider "aws" {
-  region  = "Global"
+  # Configuration options
 }
